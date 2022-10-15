00af86bf19b6e1912f2678b163efb9ded06ad290
DrHaid <40427630+DrHaid@users.noreply.github.com>
2022-10-15 23:56:55 +0200

    refactor rendering to CatPawRenderer

--- src/CatCanvas.tsx
+++ src/CatCanvas.tsx
@@ -1,12 +1,11 @@
-import React, { MouseEventHandler, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { CatPaw } from "./CatPaw";
-import { CatCanvasProps, EasingType } from "./types";
+import { CatPawRenderer } from "./CatPawRenderer";
+import { CatCanvasProps } from "./types";
 import { Vec2 } from "./Vec2";
-import { Vec2Animation } from "./Vec2Animation";
 
 export const CatCanvas = ({ height, width }: CatCanvasProps) => {
-  const catPaws = useRef<CatPaw[]>([]);
-  const catPawPrints = useRef<Vec2[]>([]);
+  const catPawRenderer = useRef<CatPawRenderer>(new CatPawRenderer());
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const ctx = useRef<CanvasRenderingContext2D | null>(null);
 
@@ -16,21 +15,8 @@ export const CatCanvas = ({ height, width }: CatCanvasProps) => {
   const renderFrame = () => {
     clearCanvas();
 
-    catPawPrints.current.forEach((print) => {
-      if (!ctx.current || !canvasRef.current) return;
-      ctx.current.fillStyle = "#0008FF";
-      ctx.current.fillRect(print.x, print.y, 25, 25);
-    })
-
-    catPaws.current.forEach((paw) => {
-      if (!ctx.current || !canvasRef.current) return;
-      paw.update(0.0035);
-      ctx.current.fillStyle = "#FF0000";
-      ctx.current.fillRect(paw.position.x, paw.position.y, 25, 25);
-      if(paw.placePawPrint()){
-        catPawPrints.current.push(paw.targetPosition)
-      }
-    })
+    if(!ctx.current) return;
+    catPawRenderer.current.renderFrame(ctx.current, 0.0035);
   };
 
   const tick = () => {
@@ -48,7 +34,7 @@ export const CatCanvas = ({ height, width }: CatCanvasProps) => {
     if(!width||!height) return;
     const screenSize = new Vec2(width, height);
     const targetPos = new Vec2(clientX, clientY);
-    catPaws.current.push(new CatPaw(screenSize, targetPos));
+    catPawRenderer.current.addCatPaw(new CatPaw(screenSize, targetPos));
   }
 
   return (
---
+++ src/CatPawRenderer.ts
@@ -0,0 +1,39 @@
+import { CatPaw } from "./CatPaw";
+import { Vec2 } from "./Vec2";
+
+export class CatPawRenderer{
+  catPaws: CatPaw[];
+  pawPrints: Vec2[];
+
+  constructor(){
+    this.catPaws = [];
+    this.pawPrints = [];
+  }
+
+  renderFrame(ctx: CanvasRenderingContext2D, frameTime: number){
+    this.drawPawPrints(ctx);
+    this.drawCatPaws(ctx, frameTime);
+  }
+
+  drawCatPaws(ctx: CanvasRenderingContext2D, delta: number){
+    this.catPaws.forEach((paw) => {
+      paw.update(delta);
+      ctx.fillStyle = "#FF0000";
+      ctx.fillRect(paw.position.x, paw.position.y, 25, 25);
+      if(paw.placePawPrint()){
+        this.pawPrints.push(paw.targetPosition)
+      }
+    })
+  }
+
+  drawPawPrints(ctx: CanvasRenderingContext2D){
+    this.pawPrints.forEach((print) => {
+      ctx.fillStyle = "#0008FF";
+      ctx.fillRect(print.x, print.y, 25, 25);
+    })
+  }
+
+  addCatPaw(catPaw: CatPaw){
+    this.catPaws.push(catPaw);
+  }
+}
