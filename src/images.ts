f147fb887109b6e92bbc846065673b9881a33559
DrHaid <40427630+DrHaid@users.noreply.github.com>
2022-10-31 20:41:37 +0100

    add catpaw and pawprint images

--- src/images.ts
+++ src/images.ts
@@ -1,3 +1,14 @@
-export const CATPAW_PATHS = [new URL("img/paw.png", import.meta.url)];
+export const CATPAW_PATHS = [
+  new URL("img/catpaw1.png", import.meta.url),
+  new URL("img/catpaw2.png", import.meta.url),
+  new URL("img/catpaw3.png", import.meta.url),
+  new URL("img/catpaw4.png", import.meta.url),
+  new URL("img/catpaw5.png", import.meta.url),
+];
 
-export const PAWPRINT_PATHS = [new URL("img/print.png", import.meta.url)];
+export const PAWPRINT_PATHS = [
+  new URL("img/pawprint1.png", import.meta.url),
+  new URL("img/pawprint2.png", import.meta.url),
+  new URL("img/pawprint3.png", import.meta.url),
+  new URL("img/pawprint4.png", import.meta.url)
+];
