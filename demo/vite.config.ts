5f363da5370ff28e7ab748d3ee2ff309ba6b40f4
DrHaid <40427630+DrHaid@users.noreply.github.com>
2023-07-31 21:41:26 +0200

    dema app base path fix

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-cat-paws/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["react-cat-paws"],
