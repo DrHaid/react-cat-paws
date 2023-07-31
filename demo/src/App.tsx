ef453d2131b235ef1d931465b9ad7af7fe6e0057
DrHaid <40427630+DrHaid@users.noreply.github.com>
2023-07-31 21:04:57 +0200

    add CatPaws and gh-pages to demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,24 +1,25 @@
 import GitHubLogo from "./assets/github.svg";
 import NPMLogo from "./assets/npm.svg";
 import "./App.css";
+import { useState } from "react";
+import { CatPaws } from "react-cat-paws";
 
 export const App = () => {
+  const [showCatPaws, setShowCatPaws] = useState(false);
+
   return (
     <>
+      {showCatPaws && (
+        <CatPaws onClose={() => setShowCatPaws(false)} fillScreen />
+      )}
       <h1>React - CatPaws🐾</h1>
-      <div>
-        <button
-          onClick={() => ({
-            /* TODO: actually add the cat paws */
-          })}
-        >
-          Click me!
-        </button>
+      <div className="container">
+        <button onClick={() => setShowCatPaws(true)}>Click me!</button>
       </div>
-      <div>
+      <div className="container">
         <pre>npm install react-cat-paws</pre>
       </div>
-      <div>
+      <div className="container">
         <a href="https://github.com/DrHaid/react-cat-paws">
           <img src={GitHubLogo} />
         </a>
